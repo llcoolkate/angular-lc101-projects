561e12242cb49ba80218fad9d382c5e521d8b0fa
LLCoolKate <107154088+llcoolkate@users.noreply.github.com>
2022-07-27 04:37:31 -0500

    safety

---
+++ lesson1/exercises/src/app/fav-links/fav-links.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'fav-links',
+  templateUrl: './fav-links.component.html',
+  styleUrls: ['./fav-links.component.css']
+})
+export class FavLinksComponent implements OnInit {
+  linksTitle = "Links";
+  favLinks = ["https://nymag.com/strategist/", "https://theuselessweb.com/"]
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Cramblin Duvet';
+  photosTitle = 'Cramblin Duvet Photos';
   image1 = 'https://www.gannett-cdn.com/-mm-/69b7aec62f5ef0463a7bc8328492b59c29e411ad/c=0-72-1644-1001/local/-/media/2018/06/14/DetroitFreeP/DetroitFreePress/636645935871371058-detroiters-02-001-16.jpg?width=660&height=373&fit=crop&format=pjpg&auto=webp';
   image2 = 'https://cdn.pastemagazine.com/www/articles/2021/03/30/detroiters_ads_main.JPG';
   image3 = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcThApmhKf-8czR0anLTkyRSCpTcRJxkXZOR2A&usqp=CAU';
